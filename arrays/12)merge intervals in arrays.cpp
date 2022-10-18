6442db9112ec34f0f62978a0707b55c34b961533
Balram Kumar <105160370+BALRAM-KUMAR@users.noreply.github.com>
2022-10-18 23:46:09 +0530

    Update 12)merge intervals in arrays.cpp

--- arrays/12)merge intervals in arrays.cpp
+++ arrays/12)merge intervals in arrays.cpp
@@ -45,3 +45,28 @@ Merged Overlapping Intervals are
 Time Complexity: O(NlogN) + O(N). O(NlogN) for sorting and O(N) for traversing through the array.
 
 Space Complexity: O(N) to return the answer of the merged intervals.
+  
+  
+  
+  one more easy solution
+  
+  
+  vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) {
+        vector<vector<int>>ans;
+        if(intervals.size() == 0)
+            return ans;
+        sort(intervals.begin(), intervals.end());
+        int l = intervals[0][0];
+        int r = intervals[0][1];
+        for(int i = 1; i < intervals.size(); i++){
+            if(intervals[i][0] <= r)
+                r = max(r, intervals[i][1]);
+            else{
+                ans.push_back({l,r});
+                l = intervals[i][0];
+                r = intervals[i][1];
+            }
+        }
+        ans.push_back({l,r});
+        return ans;
+    }
