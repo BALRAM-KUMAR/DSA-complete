89462bd1571dcaf80a813b9f1870261b0766a1d6
Balram Kumar <105160370+BALRAM-KUMAR@users.noreply.github.com>
2022-10-18 12:09:35 +0530

    Create 12)merge intervals in arrays.cpp

---
+++ arrays/12)merge intervals in arrays.cpp
@@ -0,0 +1,2 @@
+https://takeuforward.org/data-structure/merge-overlapping-sub-intervals/
+https://leetcode.com/problems/merge-intervals/discuss/1644017/C%2B%2B-EASY-TO-SOLVE-oror-Beginner-Friendly-with-Detailed-Explanations
