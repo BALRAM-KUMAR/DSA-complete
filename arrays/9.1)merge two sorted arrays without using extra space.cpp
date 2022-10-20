e0f01d022281508671e595a3765c6d470db4af3f
Balram Kumar <105160370+BALRAM-KUMAR@users.noreply.github.com>
2022-10-20 23:20:17 +0530

    Update 9.1)merge two sorted arrays without using extra space.cpp

--- arrays/9.1)merge two sorted arrays without using extra space.cpp
+++ arrays/9.1)merge two sorted arrays without using extra space.cpp
@@ -1,23 +1,3 @@
 https://takeuforward.org/data-structure/merge-two-sorted-arrays-without-extra-space/
 https://www.geeksforgeeks.org/merge-two-sorted-arrays-o1-extra-space/
- vector<vector<int>> overlappedInterval(vector<vector<int>>& intervals) {
-         // Code here
-
-  sort(intervals.begin(), intervals.end());
-  vector < vector < int >> merged;
-
-  for (int i = 0; i < intervals.size(); i++) {
-    if (merged.empty() || merged.back()[1] < intervals[i][0]) {
-      vector < int > v = {
-        intervals[i][0],
-        intervals[i][1]
-      };
-
-      merged.push_back(v);
-    } else {
-      merged.back()[1] = max(merged.back()[1], intervals[i][1]);
-    }
-  }
-
-  return merged;
-}
+ 
