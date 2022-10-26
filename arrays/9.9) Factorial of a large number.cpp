dd95fc6565426dafe41bcc54cd29766898b4dc89
Balram Kumar <105160370+BALRAM-KUMAR@users.noreply.github.com>
2022-10-26 18:24:30 +0530

    Create 9.9) Factorial of a large number.cpp

---
+++ arrays/9.9) Factorial of a large number.cpp
@@ -0,0 +1,90 @@
+solution 1:- using arrays/vectors
+
+void multiply(int n, vector<int>& number) {
+        int carry = 0;
+        for (int i = 0; i < number.size(); i++) {
+            int num = n * number[i];
+            number[i] = (char)((num + carry) % 10);
+            carry = (num + carry) / 10;
+        }
+        while (carry) {
+            number.push_back(carry % 10);
+            carry /= 10;
+        }
+    }
+    vector<int> factorial(int N){
+        vector<int> number;
+        number.push_back(1);
+        for (int i = 2; i <= N; i++) 
+            multiply(i, number);
+        reverse(number.begin(), number.end()); 
+        return number;
+    }
+
+
+
+
+solution 2:- using linkedlist
+   #include <bits/stdc++.h>
+
+using namespace std;
+
+#define rep(i, a, b) for (int i = a; i <= b; i++)
+
+using namespace std;
+// Made a class node containing data and previous pointer as
+// we are using tail pointer
+class Node {
+public:
+	int data;
+	Node* prev;
+	Node(int n)
+	{
+		data = n;
+		prev = NULL;
+	}
+};
+
+void Multiply(Node* tail, int n)
+{
+	Node *temp = tail,
+		*prevNode = tail; // Temp variable for keeping tail
+	int carry = 0;
+	while (temp != NULL) {
+		int data = temp->data * n + carry;
+		temp->data = data % 10; // stores the last digit
+		carry = data / 10;
+		prevNode = temp;
+		temp = temp->prev; // Moving temp by 1 prevNode will
+						// now denote temp
+	}
+	// If carry is greater than 0 then we create another
+	// node for it.
+	while (carry != 0) {
+		prevNode->prev = new Node((int)(carry % 10));
+		carry /= 10;
+		prevNode = prevNode->prev;
+	}
+}
+
+void print(Node* tail)
+{
+	if (tail == NULL) // Using tail recursion
+		return;
+	print(tail->prev);
+	cout
+		<< tail->data; // Print linked list in reverse order
+}
+
+// Driver code
+int main()
+{
+	int n = 20;
+	Node tail(1); // Create a node and initialise it by 1
+	rep(i, 2, n)
+		Multiply(&tail, i); // Run a loop from 2 to n and
+							// multiply with tail's i
+	print(&tail); // Print the linked list
+	cout << endl;
+	return 0;
+}
