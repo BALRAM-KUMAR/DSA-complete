7beee27074264a52fe2704e1feb6a855b702cd06
Balram Kumar <105160370+BALRAM-KUMAR@users.noreply.github.com>
2023-03-11 20:07:58 +0530

    Create Traversal.cpp

---
+++ tree/Traversal.cpp
@@ -0,0 +1,25 @@
+void preorder(TreeNode<int> *root)
+{
+  if(root==NULL)
+    return;
+  cout<<root->data<<" ";
+  for(int i=0;i<root->children.size();i++)
+  {
+    preorder(root->children[i]);
+  }
+  
+}
+
+
+
+void postorder(TreeNode<int> *root)
+{
+  if(root==NULL)
+    return;
+  for(int i=0;i<root->children.size();i++)
+  {
+    postorder(root->children[i]);
+  }
+  cout<<root->data<<" ";
+  
+}
