df545ee6625a4f6d4376f78f6a3d9e5b8b5e7789
Balram Kumar <105160370+BALRAM-KUMAR@users.noreply.github.com>
2023-03-14 01:16:05 +0530

    Create LevelwiseTraversal.cpp

---
+++ Binary Tree/LevelwiseTraversal.cpp
@@ -0,0 +1,73 @@
+template <typename T>
+
+class BinaryTreeNode
+{
+    public:
+    T data;
+    BinaryTreeNode *left;
+    BinaryTreeNode *right;
+    BinaryTreeNode(T data){
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+    ~BinaryTreeNode(){
+        delete left;
+        delete right;
+    }
+    
+};
+void printlevelwise(BinaryTreeNode<int> *root)
+{
+    cout<<root->data<<" ";
+    queue<BinaryTreeNode<int>*>pendingnodes;
+    pendingnodes.push(root);
+    while(pendingnodes.size()!=0){
+        BinaryTreeNode<int> *front=pendingnodes.front();
+        pendingnodes.pop();
+        if(front->left){
+            cout<<front->left->data<<" ";
+            pendingnodes.push(front->left);
+        }
+        if(front->right){
+            cout<<front->right->data<<" ";
+            pendingnodes.push(front->right);
+        }
+        
+    }
+}
+
+
+
+
+
+
+other syntax:- 
+  struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+}; */
+vector<int> reverseLevelOrder(Node *root)
+{
+     vector<int> output;
+    output.push_back(root->data);
+    queue<Node *>pendingnodes;
+    pendingnodes.push(root);
+    while(pendingnodes.size()!=0){
+        Node *front=pendingnodes.front();
+        pendingnodes.pop();
+        if(front->right){
+            output.push_back(front->right->data);
+            pendingnodes.push(front->right);
+        }
+        if(front->left){
+            output.push_back(front->left->data);
+            pendingnodes.push(front->left);
+        }
+        
+    }
+    reverse(output.begin(),output.end());
+    return output;
+}
