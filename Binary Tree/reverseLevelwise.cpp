d6cfaf3c29bcd98d1909a251f1e72eb6cdbc4e59
Balram Kumar <105160370+BALRAM-KUMAR@users.noreply.github.com>
2023-03-14 01:18:20 +0530

    Create reverseLevelwise.cpp

---
+++ Binary Tree/reverseLevelwise.cpp
@@ -0,0 +1,20 @@
+vector<int> reverseLevelOrder(Node *root)
+{
+     vector<int> output;
+    queue<Node *>pendingnodes;
+    pendingnodes.push(root);
+    while(pendingnodes.size()!=0){
+        Node *front=pendingnodes.front();
+        output.push_back(front->data);
+        pendingnodes.pop();
+        if(front->right){
+            pendingnodes.push(front->right);
+        }
+        if(front->left){
+            pendingnodes.push(front->left);
+        }
+        
+    }
+    reverse(output.begin(),output.end());
+    return output;
+}
