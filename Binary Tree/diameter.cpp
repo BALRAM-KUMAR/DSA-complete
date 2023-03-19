3a5e05549b3b31d52de96bbe389a08d4bb7c7750
Balram Kumar <105160370+BALRAM-KUMAR@users.noreply.github.com>
2023-03-20 01:00:11 +0530

    Update diameter.cpp

--- Binary Tree/diameter.cpp
+++ Binary Tree/diameter.cpp
@@ -1,3 +1,10 @@
+Diameter of tree :- longest path between 2 node
+                    path does not need to pass via root
+
+approach1:- coding ninja brute force TC O(N) where we find both maximum left and right length and take max
+approach2:- 
+ TC=o(n) and sc=O(n) assuming auxialary stack space
+ 
 int maxi;
 class Solution {
   public:
@@ -7,10 +14,10 @@ class Solution {
         {
             return 0;
         }
-        int x=fun(root->left);
-        int y=fun(root->right);
-        maxi=max(maxi,x+y+1);
-        return 1+max(x,y);
+        int lh=fun(root->left);
+        int rh=fun(root->right);
+        maxi=max(maxi,lh+rh+1);
+        return 1+max(lh,rh);
     }
     int diameter(Node* root) {
          if(root==NULL)
