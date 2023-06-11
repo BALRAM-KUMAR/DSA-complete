52eadca78ab71fdb69c44512103004862bf4a13b
Balram Kumar <105160370+BALRAM-KUMAR@users.noreply.github.com>
2023-06-11 20:14:06 +0530

    Update 4)move_all_negative_end_of_array.cpp

--- arrays/4)move_all_negative_end_of_array.cpp
+++ arrays/4)move_all_negative_end_of_array.cpp
@@ -28,3 +28,15 @@ Output :
              arr[i]=a[i];
          }    
     }
+Approach 2:-//Two pointer approach
+  void rearrange(int arr[], int n)
+{
+    int j = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < 0) {
+            if (i != j)
+                swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+}
