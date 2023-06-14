dd5b603984aba371951b1a3eaabfc80d8d5782dd
Balram Kumar <105160370+BALRAM-KUMAR@users.noreply.github.com>
2023-06-14 08:38:59 +0530

    Update 9.9.8 next permutation.cpp

--- arrays/9.9.8 next permutation.cpp
+++ arrays/9.9.8 next permutation.cpp
@@ -26,4 +26,60 @@ Approach 2:- use stl function
 
   
   Approach 3:-o(N)
-    
+   1. longer perfix match  (back side){arr[i]<a[i+1]}
+                  find the break point
+   2. find>1 but the smallest one that u say close
+   3. try to place in sorted array
+   
+   
+   example:- 
+      arr[]={2 1 5 4 3 0 0 }
+               ^  |.........|
+1.             break     longest prefix match
+             point
+             ind=i;
+2. now swap which is slightly greater
+updatation 2 3 5 4 1 0 0  
+3. reverse ind to n-1
+nxt per:- 2 3 0 0 1 4 5
+  
+code:- 
+  for(int i=n-2;i>=0;i--)
+  {
+    if(arr[i]<arr[i+1]){
+       ind=i;
+       break;
+    }
+  }
+//edge case
+if(ind==-1)
+  return arr;
+
+for(int i=n-1;i>=ind;i--)
+{
+  if(arr[i]>arr[ind]
+     {
+       swap(arr[i],arr[ind])
+         break;
+     }
+}
+     
+reverse(arr,ind+1,n-1)
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
